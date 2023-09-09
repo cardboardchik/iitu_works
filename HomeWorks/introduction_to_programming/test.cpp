213982fccae4f2be50c2359b3c768efe73749915
Karton4ik <artyrzzo9@gmail.com>
2023-09-09 11:28:42 +0600

    Create test.cpp

---
+++ HomeWorks/introduction_to_programming/test.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // integer 1
+    /*int L;
+    int answer;
+
+    cin >> L;
+
+    answer = L / 100;
+
+    cout << answer;
+    */
+
+   // integer 2
+    /*int M;
+    int answer;
+
+    cin >> M;
+
+    answer = M / 1000;
+
+    cout << answer;
+    */
+
+   //integer 3
+    /*int L;
+    int answer;
+
+    cin >> L;
+
+    answer = L / 1024;
+
+    cout << answer;
+    */
+
+   //integer 4
+    /*int A;
+    int B;
+    int answer;
+
+    cin >> A;
+    cin >> B;
+
+    answer = A / B;
+
+    cout << answer;
+    */
+
+   //integer 5
+    /*int A;
+    int B;
+    int answer;
+
+    cin >> A;
+    cin >> B;
+
+    answer = A % B;
+
+    cout << answer;
+    */
+
+
+    //integer 6
+    /*int i;
+    int tens;
+    int ones;
+
+    cin >> i;
+
+    tens = i / 10;
+    ones = i % 10;
+
+    cout << tens;
+    cout << ones;
+    */
+
+
+    //integer 7
+    /*int i;
+    int tens;
+    int ones;
+    int sum;
+
+    cin >> i;
+
+    tens = i / 10;
+    ones = i % 10;
+
+    sum = tens + ones;
+
+    cout << sum;
+    */
+
+
+   //integer 8
+    /*int i;
+    int tens;
+    int ones;
+    int answer;
+
+    cin >> i;
+
+    tens = i / 10;
+    ones = i % 10;
+
+    answer = (ones * 10) + (tens);
+
+    cout << answer; 
+    */
+    return 0;
+
+
+}
+
