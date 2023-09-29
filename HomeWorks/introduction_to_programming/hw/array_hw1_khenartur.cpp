a2941911b3f0e297a8d224617d443f04fcb22195
Karton4ik <artyrzzo9@gmail.com>
2023-09-29 16:05:15 +0600

    Create array_hw1_khenartur.cpp

---
+++ HomeWorks/introduction_to_programming/hw/array_hw1_khenartur.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // 1
+    /*int arr[14];
+    int answer = 0;
+
+    for (int i = 0; i < 14; i++){
+        cin >> arr[i];
+        if ((arr[i] % 2) == 0){
+            answer++;
+        }
+    }
+
+    cout << answer;
+    */
+
+    // 2
+    /*int arr[12];
+    int sum = 0;
+
+    for (int i = 0; i < 12; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    arr[4] = sum / 12;
+    
+    for (int i = 0; i < 12; i++){
+        cout << arr[i] << " ";
+    }
+    */
+
+    // 3
+    /*int arr[11];
+    double sum = 0;
+    double mida;
+    int answer = 0;
+
+    for (int i = 0; i < 11; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    mida = sum / 11;
+    
+    for (int i = 0; i < 11; i++){
+        if (arr[i] > mida){
+            answer++;
+        }
+    }
+    cout << answer;
+    */
+
+
+    // 4
+
+
+    
+    return 0;
+}
