cee51e68ab5ab0d24d65e148b021518c0b5c6fdb
Karton4ik <artyrzzo9@gmail.com>
2023-11-02 17:57:20 +0600

    Update practice8.js

--- HomeWorks/web/practices/js/practice8.js
+++ HomeWorks/web/practices/js/practice8.js
@@ -1,32 +1,30 @@
 
 // Task 1
 // function describeCountry(country, population, capitalCity) {
-//     document.write(`${country} has ${population} people and its capital city is ${capitalCity}`);
+//     document.write(`${country} has ${population} milion people and its capital city is ${capitalCity}<br>`);
 // }
 
-// let country = prompt();
-// let population = prompt();
-// let capital = prompt();
 
-// describeCountry(country, population, capital);
+// describeCountry("Kazakhstan", 19, "Astana");
+// describeCountry("USA", 310, "Washington");
+// describeCountry("China", 1441, "Beijing");
 
 // Task 2
 // function percentageOfWorld1(population){
-//     document.write(`${~~((population/7900) * 100)}% of the world population`);
+//     document.write(`${~~((population/7900) * 100)}% of the world population<br>`);
 // }
 
-// let population = prompt();
-
-// percentageOfWorld1(population);
+// percentageOfWorld1(1443);
+// percentageOfWorld1(443);
+// percentageOfWorld1(2343);
 
 // const percentageOfWorld2 = function(population) {
-//     return ((population/7900) * 100)
+//     document.write(`${~~((population/7900) * 100)}% of the world population<br>`)
 // }
 
-// let population1 = prompt();
-
-// percentageOfWorld2(population1);
-
+// percentageOfWorld2(1443);
+// percentageOfWorld2(443);
+// percentageOfWorld2(2343);
 
 // Task 3
 // let population = prompt();
@@ -63,20 +61,26 @@
 // document.write(days_of_a_year(2024))
 
 // Task 6
-function calcAverage(firstScore, secondScore, thirdScore){
-    return (~~((firstScore + secondScore + thirdScore) / 3));
-}
+// function calcAverage(firstScore, secondScore, thirdScore){
+//     return (~~((firstScore + secondScore + thirdScore) / 3));
+// }
 
-let yesyl_avg_score = calcAverage(44, 23, 71);
-let yertys_avg_score = calcAverage(65, 54, 49);
+// let yesyl_avg_score1 = calcAverage(44, 23, 71);
+// let yertys_avg_score1 = calcAverage(65, 54, 49);
+// let yesyl_avg_score2 = calcAverage(85, 54, 41);
+// let yertys_avg_score2 = calcAverage(23, 34, 27);
 
-function checkWinner(avgYesyl, avgYertys){
-    if (avgYesyl > avgYertys){
-        document.write(`Yesyl win (${avgYesyl} and ${avgYertys})`)
-    }
-    else{
-        document.write(`Yertys win (${avgYesyl} and ${avgYertys})`)
-    }
-}
+// function checkWinner(avgYesyl, avgYertys){
+//     if (avgYesyl > avgYertys * 2){
+//         document.write(`Yesyl win (${avgYesyl} and ${avgYertys})<br>`)
+//     }
+//     else if (avgYesyl * 2 < avgYertys){
+//         document.write(`Yertys win (${avgYesyl} and ${avgYertys})<br>`)
+//     }
+//     else{
+//         document.write("No one win<br>")
+//     }
+// }
 
-checkWinner(yesyl_avg_score, yertys_avg_score);
+// checkWinner(yesyl_avg_score1, yertys_avg_score1);
+// checkWinner(yesyl_avg_score2, yertys_avg_score2);
