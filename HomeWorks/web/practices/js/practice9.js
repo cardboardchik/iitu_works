62346ba9804757f4fc4f37888372fb90a0fd7ed0
Karton4ik <artyrzzo9@gmail.com>
2023-11-14 16:05:19 +0600

    practice9

---
+++ HomeWorks/web/practices/js/practice9.js
@@ -0,0 +1,21 @@
+// task 1
+// let populations = [1234, 123, 4321, 90];
+
+// if (populations.length == 4){
+//     document.write(true + "<br>");
+// }
+// else{
+//     document.write(false + "<br>");
+// }
+
+// function percentageOfWorld1(population){
+//     document.write(`${~~((population/7900) * 100)}% of the world population<br>`);
+// }
+
+// for (let i = 0; i < populations.length; i++){
+//     percentageOfWorld1(populations[i]);
+// }
+
+
+// task 2
+let neighbours = ["Canada"]
